a11cb22fe229b0085fdc95c87c5d3fee7312d5ed
jorgiyo <38118344+jorgiyo@users.noreply.github.com>
2019-08-08 08:49:49 +0200

    Añadimos atributos al shortcode

--- copy_al_pie.php
+++ copy_al_pie.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Copy al pie
 Description: Copyright a pie de página
-Version:     1.0
+Version:     1.1
 Author:      jorgiyo
 Author URI:  http://peonnegro.com
 License:     GPL2
@@ -10,9 +10,12 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/jorgiyo/copy_al_pie
 */
 
-function imp_copy(){
+function imp_copy($grupo){
+  $a = shortcode_atts( array(
+		'grupo' => 'jorgiyo',
+	), $atts );
 echo "<div class='copy' align='center'>";
-echo "&copy".date('Y')." - El Capitán Elefante - Web: <a href=https://bideoklip.com>bideoklip.com</a><br><a href=https://www.elcapitanelefante.com/politica-de-privacidad/>Política de privacidad</a>";
+echo "&copy".date('Y')." - ".$a['grupo']." - Web: <a href=https://bideoklip.com>bideoklip.com</a><br><a href=https://www.elcapitanelefante.com/politica-de-privacidad/>Política de privacidad</a>";
 echo "</div>";
 }
 add_shortcode( 'copy','imp_copy');
